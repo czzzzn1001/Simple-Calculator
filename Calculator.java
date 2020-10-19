1bbd7172c22808d36edc42490d6985658ce6922f
czzzzn1001 <56014842+czzzzn1001@users.noreply.github.com>
2020-10-18 21:17:47 -0400

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,39 +1,42 @@
+package com.shiyanlou.calculator;
 
-
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import java.util.Vector;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.math.BigDecimal;
+import java.util.Vector;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
 
 public class Calculator {
+	String str1 = "0";
 
-	String str1 = "0"; 
-
-	String str2 = "0"; 
 	
-	String signal = "+"; 
-	
+	String str2 = "0";
 
+
+	String signal = "+";
+
+	
 	String result = "";
-
-	
-	int k1 = 1;
-
-	int k2 = 1;
-
-	int k3 = 1;
-
-	int k4 = 1;
-
-	int k5 = 1;
-
-	JButton store; 
-	
+	int k1=1;
+	int k2=1;
+	int k3=1;
+	int k4=1;
+	int k5=1;
+	JButton store;
 	@SuppressWarnings("rawtypes")
 	Vector vt = new Vector(20, 10);
-
-
+	
 	JFrame frame = new JFrame("Calculator");
 	JTextField result_TextField = new JTextField(result, 20);
 	JButton clear_Button = new JButton("Clear");
@@ -53,18 +56,10 @@ public class Calculator {
 	JButton button_cheng = new JButton("*");
 	JButton button_chu = new JButton("/");
 	JButton button_dy = new JButton("=");
-
-
+    
 	public Calculator() {
-	
-
 		button0.setMnemonic(KeyEvent.VK_0);
-
-
-
 		result_TextField.setHorizontalAlignment(JTextField.RIGHT);
-
-
 		JPanel pan = new JPanel();
 		pan.setLayout(new GridLayout(4, 4, 5, 5));
 		pan.add(button7);
@@ -89,18 +84,13 @@ public class Calculator {
 		pan2.setLayout(new BorderLayout());
 		pan2.add(result_TextField, BorderLayout.WEST);
 		pan2.add(clear_Button, BorderLayout.EAST);
-
 		frame.setLocation(300, 200);
-
 		frame.setResizable(false); 
 		frame.getContentPane().setLayout(new BorderLayout());
 		frame.getContentPane().add(pan2, BorderLayout.NORTH);
 		frame.getContentPane().add(pan, BorderLayout.CENTER);
-
 		frame.pack();
 		frame.setVisible(true);
-
-	
 		class Listener implements ActionListener {
 			@SuppressWarnings("unchecked")
 			public void actionPerformed(ActionEvent e) {
@@ -110,20 +100,19 @@ public class Calculator {
 				if (k1 == 1) {
 					if (k3 == 1) {
 						str1 = "";
-	
 						k5 = 1;
 					}
 					str1 = str1 + ss;
 
 					k3 = k3 + 1;
-
+                    
+                   
 					result_TextField.setText(str1);
 
 				} else if (k1 == 2) {
 					if (k4 == 1) {
 						str2 = "";
-						
-
+					
 						k5 = 1; 
 					}
 					str2 = str2 + ss;
@@ -134,7 +123,6 @@ public class Calculator {
 			}
 		}
 
-
 		class Listener_signal implements ActionListener {
 			@SuppressWarnings("unchecked")
 			public void actionPerformed(ActionEvent e) {
@@ -143,7 +131,7 @@ public class Calculator {
 				vt.add(store);
 
 				if (k2 == 1) {
-
+				    
 					k1 = 2;
 					k5 = 1;
 					signal = ss2;
@@ -160,7 +148,6 @@ public class Calculator {
 					{
 						cal();
 						str1 = result;
-
 						k1 = 2;
 						k5 = 1;
 						k4 = 1;
@@ -172,7 +159,6 @@ public class Calculator {
 
 			}
 		}
-
 
 		class Listener_clear implements ActionListener {
 			@SuppressWarnings("unchecked")
@@ -193,7 +179,6 @@ public class Calculator {
 			}
 		}
 
-
 		class Listener_dy implements ActionListener {
 			@SuppressWarnings("unchecked")
 			public void actionPerformed(ActionEvent e) {
@@ -201,8 +186,6 @@ public class Calculator {
 				store = (JButton) e.getSource();
 				vt.add(store);
 				cal();
-				
-
 				k1 = 1; 
 				k2 = 1;
 				k3 = 1;
@@ -212,7 +195,6 @@ public class Calculator {
 			}
 		}
 		
-
 		class Listener_xiaos implements ActionListener {
 			@SuppressWarnings("unchecked")
 			public void actionPerformed(ActionEvent e) {
@@ -223,20 +205,18 @@ public class Calculator {
 					if (k1 == 1) {
 						if (k3 == 1) {
 							str1 = "";
-
 							k5 = 1; 
 						}
 						str1 = str1 + ss2;
 
 						k3 = k3 + 1;
 
-  
+                    
 						result_TextField.setText(str1);
 
 					} else if (k1 == 2) {
 						if (k4 == 1) {
 							str2 = "";
-
 							k5 = 1;
 						}
 						str2 = str2 + ss2;
@@ -251,19 +231,18 @@ public class Calculator {
 			}
 		}
 
-
 		Listener_dy jt_dy = new Listener_dy();
 		
-
+	
 		Listener jt = new Listener();
-
+		
 		Listener_signal jt_signal = new Listener_signal();
-
+	
 		Listener_clear jt_c = new Listener_clear(); 
-
+	
 		Listener_xiaos jt_xs = new Listener_xiaos();
 
-		button7.addActionListener(jt);
+		button7.addActionListener((ActionListener) jt);
 		button8.addActionListener(jt);
 		button9.addActionListener(jt);
 		button_chu.addActionListener(jt_signal);
@@ -280,31 +259,27 @@ public class Calculator {
 		button_dy.addActionListener(jt_dy);
 		button_jia.addActionListener(jt_signal);
 		clear_Button.addActionListener(jt_c);
-
-
-		frame.addWindowListener(new WindowAdapter() {
+      	frame.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
 			}
 		});
-
 	}
-
-	// è®¡ç®—é€»è¾‘
+	@SuppressWarnings("deprecation")
 	public void cal() {
-    	// æ“ä½œæ•°1
 		double a2;
-		// æ“ä½œæ•°2
+	
 		double b2;
-		// è¿ç®—ç¬¦
+		
 		String c = signal;
-		// è¿ç®—ç»“æžœ
+		
 		double result2 = 0;
 
 		if (c.equals("")) {
 			result_TextField.setText("Please input operator");
 
 		} else {
+   
 			if (str1.equals("."))
 				str1 = "0.0";
 			if (str2.equals("."))
@@ -340,6 +315,7 @@ public class Calculator {
 
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
+	
 	try {
 			UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
 		} catch (Exception e) {
@@ -347,5 +323,4 @@ public class Calculator {
 		}
 		Calculator cal = new Calculator();
 	}
-
 }
